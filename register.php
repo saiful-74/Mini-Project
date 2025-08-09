dd6d9a91f8c8685a1469b9cb310d5ad44bce79ef
saiful-74 <saifulmaim2020@gmail.com>
2025-08-09 15:38:39 +0600

    php

--- register.php
+++ register.php
@@ -1,11 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+include 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $role = $_POST['role'];
+
+    $sql = "INSERT INTO users (username, password, role) VALUES ('$username', '$password', '$role')";
+    if ($conn->query($sql)) {
+        echo "Registration successful. <a href='login.php'>Login here</a>";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+?>
+
+<form method="POST">
+    Username: <input type="text" name="username" required><br><br>
+    Password: <input type="password" name="password" required><br><br>
+    Role: 
+    <select name="role" required>
+        <option value="admin">Admin</option>
+        <option value="user">User</option>
+    </select><br><br>
+    <input type="submit" value="Register">
+</form>
